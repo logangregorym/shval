497779800d06d3b1023f8cf5d835966bde6e30b4
Logan Moody <moodylg@dukes.jmu.edu>
2018-03-14 18:02:40 -0400

    add func data to relative diff colored script

--- scripts/filter_diff_relative.rb
+++ scripts/filter_diff_relative.rb
@@ -22,12 +22,12 @@
 
 # a single differential trace computational graph node
 class DiffNode
-  attr_reader :id, :label, :abserr, :relerr, :addr, :disas, :src
+  attr_reader :id, :label, :abserr, :relerr, :addr, :disas, :func, :src
   attr_accessor :in, :out, :color
 
-  def initialize (id, label, abserr, relerr, addr="", disas="", src="")
+  def initialize (id, label, abserr, relerr, addr="", disas="", func="", src="")
     @id, @label, @abserr, @relerr = id, label, abserr, relerr
-    @addr, @disas, @src = addr, disas, src
+    @addr, @disas, @func, @src = addr, disas, func, src
     @in, @out = [], []
     @color = 0xff;
   end
@@ -37,7 +37,7 @@ class DiffNode
     cformat = "#{@color.to_s(16).rjust(2, '0')}"
     "#{@id.to_s} [label=\"#{@label.to_s}" +
     " abserr=#{@abserr.to_s} relerr=#{@relerr.to_s}"+
-    " addr=#{@addr} disas='#{@disas}' src=#{@src}\"" +
+    " addr=#{@addr} disas='#{@disas}' func='#{@func}' src=#{@src}\"" +
     " style=filled fillcolor=\"\#ff" + cformat + cformat + "\"];\n" +
     @out.map { |id| "#{@id} -> #{id};" }.join("\n")
   end
@@ -51,8 +51,8 @@ edges = []            # list of [src,dst] pairs
 # load graph from DOT file
 
 ARGF.each_line do |line|
-  if line =~ /^(\d+) \[label="([^ ]*) abserr=([^ ]*) relerr=([^ ]*) addr=([0-9a-f]*) disas='([^']*)' src=([^ ]*)"\];$/
-    graph[$1.to_i] = DiffNode.new($1.to_i, $2, $3.to_f, $4.to_f, $5, $6, $7)
+  if line =~ /^(\d+) \[label="([^ ]*) abserr=([^ ]*) relerr=([^ ]*) addr=([0-9a-f]*) disas='([^']*)' func='([^']*)' src=([^ ]*)"\];$/
+    graph[$1.to_i] = DiffNode.new($1.to_i, $2, $3.to_f, $4.to_f, $5, $6, $7, $8)
   elsif line =~ /^(\d+) \[label="([^ ]*) abserr=([^ ]*) relerr=([^ ]*)"\];$/
     graph[$1.to_i] = DiffNode.new($1.to_i, $2, $3.to_f, $4.to_f)
   elsif line =~ /^(\d+) -> (\d+);$/
